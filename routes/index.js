7bb7129b4dd451b7ad2fed27e1f07c087e1dc4aa
Assane17 <a.dione@iatf.it>
2022-07-27 12:57:23 +0000

    change file enter

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../database/db');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/',  function (req, res, next) {
+    db.serialize(() => {
+        db.all("SELECT * FROM project", (err, rows) => {
+            if (err) {
+                throw err;
+            }else{
+                res.render('index', { title: 'Express', datas: rows });
+            }
+
+        });
+    });
+
+
 });
 
 module.exports = router;
